39910d86fadd67352f96fe85276c408a7b69e6af
SparrowBrain <qwxmail@gmail.com>
2023-01-05 23:13:27 +0200

    Added recently played filter

---
+++ PlayNext.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlayNext.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlayNext.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0cb72e7f-6fec-43f4-bb47-00c6a0f5e4b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PlayNext.UnitTests/RecentlyPlayedFilterTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.Xunit2;
+using Moq;
+using Playnite.SDK.Models;
+using Xunit;
+
+namespace PlayNext.UnitTests
+{
+    public class RecentlyPlayedFilterTests
+    {
+        [Theory, AutoMoqData]
+        public void Filter_ReturnsOneGame_When_OnlyOneGameIsWithinRecentDayCount(
+            [Frozen] Mock<IDateTimeProvider> dateTimeProviderMock,
+            DateTime now,
+            int recentDayCount,
+            Game[] games,
+            RecentlyPlayedFilter sut)
+        {
+            // Arrange
+            dateTimeProviderMock.Setup(x => x.GetNow()).Returns(now);
+            games.First().LastActivity = now - TimeSpan.FromDays(recentDayCount - 1);
+            foreach (var game in games.Skip(1))
+            {
+                game.LastActivity = now - TimeSpan.FromDays(recentDayCount + 1);
+            }
+
+            // Act
+            var recentGames = sut.Filter(games, recentDayCount);
+
+            // Assert
+            var recentGame = Assert.Single(recentGames);
+            Assert.Equal(games[0], recentGame);
+        }
+    }
+
+    public interface IDateTimeProvider
+    {
+        DateTime GetNow();
+    }
+
+    public class RecentlyPlayedFilter
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public RecentlyPlayedFilter(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public IEnumerable<Game> Filter(IEnumerable<Game> games, int recentDayCount)
+        {
+            return games.Where(x => x.LastActivity >= _dateTimeProvider.GetNow() - TimeSpan.FromDays(recentDayCount));
+        }
+    }
+}
