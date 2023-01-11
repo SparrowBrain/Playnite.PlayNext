4b32f05d56ea25b2f2c0a29abb213095d299ffce
SparrowBrain <qwxmail@gmail.com>
2023-01-11 23:26:34 +0200

    Added score normalizer

---
+++ PlayNext/Score/ScoreNormalizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayNext.Score
+{
+    public class ScoreNormalizer
+    {
+        public IDictionary<Guid, float> Normalize(Dictionary<Guid, float> scores)
+        {
+            var max = scores.Max(x => x.Value);
+            return scores.ToDictionary(x => x.Key, x => x.Value * 100 / max);
+        }
+    }
+}
