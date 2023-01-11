cff4b16ec285653825efa517c1da544b6d6377f0
SparrowBrain <qwxmail@gmail.com>
2023-01-11 23:01:00 +0200

    Added GameSocreByAttributeCalculator

---
+++ PlayNext.UnitTests/Score/GameScoreByAttributeCalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.Xunit2;
+using PlayNext.Score;
+using Playnite.SDK.Models;
+using Xunit;
+
+namespace PlayNext.UnitTests.Score
+{
+    public class GameScoreByAttributeCalculatorTests
+    {
+        [Theory]
+        [InlineAutoData(nameof(Game.GenreIds))]
+        [InlineAutoData(nameof(Game.CategoryIds))]
+        [InlineAutoData(nameof(Game.DeveloperIds))]
+        [InlineAutoData(nameof(Game.PublisherIds))]
+        [InlineAutoData(nameof(Game.TagIds))]
+        public void Calculate_ReturnsGameWithAttributeScore_When_1GameAnd1Attribute(
+            string attributeIdsName,
+            Dictionary<Guid, float> attributeScore,
+            Game game,
+            GameScoreByAttributeCalculator sut)
+        {
+            // Arrange
+            var games = new[] { game };
+            var attributeInGame = attributeScore.Keys.First();
+            SetAttributes(attributeIdsName, game, attributeInGame);
+            var attributeSelector = new Func<Game, IEnumerable<Guid>>(x => (IEnumerable<Guid>)x.GetType().GetProperty(attributeIdsName).GetValue(x));
+
+            // Act
+            var result = sut.Calculate(games, attributeSelector, attributeScore);
+
+            // Assert
+            var gameWithScore = Assert.Single(result);
+            Assert.Equal(attributeScore[attributeInGame], gameWithScore.Value);
+            Assert.Equal(game.Id, gameWithScore.Key);
+        }
+
+        [Theory]
+        [InlineAutoData(nameof(Game.GenreIds))]
+        [InlineAutoData(nameof(Game.CategoryIds))]
+        [InlineAutoData(nameof(Game.DeveloperIds))]
+        [InlineAutoData(nameof(Game.PublisherIds))]
+        [InlineAutoData(nameof(Game.TagIds))]
+        public void Calculate_ReturnsSumOfAttributesScore_When_1GameAndManyAttributes(
+            string attributeIdsName,
+            Dictionary<Guid, float> attributeScore,
+            Game game,
+            GameScoreByAttributeCalculator sut)
+        {
+            // Arrange
+            var games = new[] { game };
+            SetAttributes(attributeIdsName, game, attributeScore.Keys.ToArray());
+            var attributeSelector = new Func<Game, IEnumerable<Guid>>(x => (IEnumerable<Guid>)x.GetType().GetProperty(attributeIdsName).GetValue(x));
+
+            // Act
+            var result = sut.Calculate(games, attributeSelector, attributeScore);
+
+            // Assert
+            var gameWithScore = Assert.Single(result);
+            Assert.Equal(attributeScore.Values.Sum(), gameWithScore.Value);
+            Assert.Equal(game.Id, gameWithScore.Key);
+        }
+
+        private static void SetAttributes(string attributeIdsName, Game game, params Guid[] attributeIds)
+        {
+            game.GetType().GetProperty(attributeIdsName).SetValue(game, new List<Guid>(attributeIds));
+        }
+    }
+}
---
+++ PlayNext/Score/GameScoreByAttributeCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Playnite.SDK.Models;
+
+namespace PlayNext.Score
+{
+    public class GameScoreByAttributeCalculator
+    {
+        public IDictionary<Guid, float> Calculate(IEnumerable<Game> games, Func<Game, IEnumerable<Guid>> attributeSelector, Dictionary<Guid, float> attributeScore)
+        {
+            var gamesWithScore = new Dictionary<Guid, float>();
+            foreach (var game in games)
+            {
+                var score = 0f;
+                foreach (var attributeId in attributeSelector.Invoke(game))
+                {
+                    if (attributeScore.ContainsKey(attributeId))
+                    {
+                        score += attributeScore[attributeId];
+                    }
+                }
+
+                gamesWithScore.Add(game.Id, score);
+            }
+
+            return gamesWithScore;
+        }
+    }
+}
