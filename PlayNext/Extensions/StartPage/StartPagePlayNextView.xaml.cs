06a966744a556822d73b7798ddc2f3549fb3ad9b
SparrowBrain <qwxmail@gmail.com>
2024-06-02 10:54:38 +0300

    Fixed Playnite crash when StartPage settings are loaded slower than UI

--- PlayNext/Extensions/StartPage/StartPagePlayNextView.xaml.cs
+++ PlayNext/Extensions/StartPage/StartPagePlayNextView.xaml.cs
@@ -1,102 +1,108 @@
-﻿using System;
+﻿using PlayNext.Settings;
+using PlayNext.ViewModels;
+using Playnite.SDK;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using PlayNext.Settings;
-using PlayNext.ViewModels;
-using Playnite.SDK;
 
 namespace PlayNext.Extensions.StartPage
 {
-    /// <summary>
-    /// Interaction logic for StartPagePlayNextView.xaml
-    /// </summary>
-    public partial class StartPagePlayNextView : UserControl
-    {
-        private const int TextHeight = 2 * 25;
-        private const int CoverMargin = 2 * 8;
-        private ILogger _logger = LogManager.GetLogger(nameof(StartPagePlayNextView));
-        private int _minCoverCount;
+	/// <summary>
+	/// Interaction logic for StartPagePlayNextView.xaml
+	/// </summary>
+	public partial class StartPagePlayNextView : UserControl
+	{
+		private const int TextHeight = 2 * 25;
+		private const int CoverMargin = 2 * 8;
+		private ILogger _logger = LogManager.GetLogger(nameof(StartPagePlayNextView));
+		private int _minCoverCount;
 
-        public StartPagePlayNextView(StartPagePlayNextViewModel viewModel, PlayNextSettings settings)
-        {
-            DataContext = viewModel;
-            UpdateMinCoverCount(settings);
-            InitializeComponent();
-        }
+		public StartPagePlayNextView(StartPagePlayNextViewModel viewModel, PlayNextSettings settings)
+		{
+			DataContext = viewModel;
+			UpdateMinCoverCount(settings);
+			InitializeComponent();
+		}
 
-        public void UpdateMinCoverCount(PlayNextSettings settings)
-        {
-            _minCoverCount = settings.StartPageMinCoverCount;
-            UpdateCoversColumnWidth();
-        }
+		public void UpdateMinCoverCount(PlayNextSettings settings)
+		{
+			_minCoverCount = settings.StartPageMinCoverCount;
+			UpdateCoversColumnWidth();
+		}
 
-        private void ListBoxItem_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (sender is ListBoxItem item)
-            {
-                if (item.DataContext is GameToPlayViewModel model)
-                {
-                    model.OpenDetails.Execute(null);
-                }
-            }
-        }
+		private void ListBoxItem_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if (sender is ListBoxItem item)
+			{
+				if (item.DataContext is GameToPlayViewModel model)
+				{
+					model.OpenDetails.Execute(null);
+				}
+			}
+		}
 
-        private void Dock_OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            var dock = sender as FrameworkElement;
-            UpdateCoversColumnWidth(dock);
-        }
+		private void Dock_OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			var dock = sender as FrameworkElement;
+			UpdateCoversColumnWidth(dock);
+		}
 
-        private void OnCoverListMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (!(sender is IScrollInfo scrollInfo))
-            {
-                _logger.Warn("Mouse wheel scroll triggered from non-scrolling control.");
-                return;
-            }
+		private void OnCoverListMouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			if (!(sender is IScrollInfo scrollInfo))
+			{
+				_logger.Warn("Mouse wheel scroll triggered from non-scrolling control.");
+				return;
+			}
 
-            for (var i = 0; i < Math.Abs(e.Delta); i++)
-            {
-                if (e.Delta < 0)
-                {
-                    scrollInfo.LineRight();
-                }
-                else
-                {
-                    scrollInfo.LineLeft();
-                }
-            }
-        }
+			for (var i = 0; i < Math.Abs(e.Delta); i++)
+			{
+				if (e.Delta < 0)
+				{
+					scrollInfo.LineRight();
+				}
+				else
+				{
+					scrollInfo.LineLeft();
+				}
+			}
+		}
 
-        private void UpdateCoversColumnWidth()
-        {
-            var dock = FindName("CoversDock") as FrameworkElement;
-            UpdateCoversColumnWidth(dock);
-        }
+		private void UpdateCoversColumnWidth()
+		{
+			var dock = FindName("CoversDock") as FrameworkElement;
+			UpdateCoversColumnWidth(dock);
+		}
 
-        private void UpdateCoversColumnWidth(FrameworkElement dock)
-        {
-            var column = FindName("CoverListWidth") as ColumnDefinition;
-            if (dock == null || column == null)
-            {
-                _logger.Warn("Dock or column is invalid");
-                return;
-            }
+		private void UpdateCoversColumnWidth(FrameworkElement dock)
+		{
+			var column = FindName("CoverListWidth") as ColumnDefinition;
+			if (dock == null || column == null)
+			{
+				_logger.Warn("Dock or column is invalid");
+				return;
+			}
 
-            var coverWidth = LandingPageExtension.Instance.Settings.MaxCoverWidth;
+			if (LandingPageExtension.Instance?.Settings == null)
+			{
+				_logger.Warn("No start page settings loaded for column width update.");
+				return;
+			}
 
-            var dynamicWidth = (Math.Floor((dock.ActualWidth - TextHeight) / (coverWidth + CoverMargin)) * (coverWidth + CoverMargin));
-            var minWidth = _minCoverCount * (coverWidth + CoverMargin);
+			var coverWidth = LandingPageExtension.Instance.Settings.MaxCoverWidth;
 
-            var newWidth = Math.Max(minWidth, dynamicWidth);
-            if (newWidth <= 0)
-            {
-                return;
-            }
+			var dynamicWidth = Math.Floor((dock.ActualWidth - TextHeight) / (coverWidth + CoverMargin)) * (coverWidth + CoverMargin);
+			var minWidth = _minCoverCount * (coverWidth + CoverMargin);
 
-            column.Width = new GridLength(newWidth, GridUnitType.Pixel);
-        }
-    }
+			var newWidth = Math.Max(minWidth, dynamicWidth);
+			if (newWidth <= 0)
+			{
+				return;
+			}
+
+			column.Width = new GridLength(newWidth, GridUnitType.Pixel);
+		}
+	}
 }
--- PlayNext/PlayNext.cs
+++ PlayNext/PlayNext.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media;
-using PlayNext.Extensions.GameActivity;
+﻿using PlayNext.Extensions.GameActivity;
 using PlayNext.Extensions.HowLongToBeat;
 using PlayNext.Extensions.StartPage;
 using PlayNext.Infrastructure.Services;
@@ -17,211 +11,217 @@ using Playnite.SDK;
 using Playnite.SDK.Events;
 using Playnite.SDK.Plugins;
 using StartPage.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace PlayNext
 {
-    public class PlayNext : GenericPlugin, IStartPageExtension
-    {
-        private readonly ILogger _logger = LogManager.GetLogger();
-        private readonly GameActivityExtension _gameActivities;
-        private readonly HowLongToBeatExtension _howLongToBeatExtension;
-        private readonly StartupSettingsValidator _startupSettingsValidator;
-        private readonly TotalScoreCalculator _totalScoreCalculator;
-        private readonly DateTimeProvider _dateTimeProvider = new DateTimeProvider();
+	public class PlayNext : GenericPlugin, IStartPageExtension
+	{
+		private readonly ILogger _logger = LogManager.GetLogger();
+		private readonly GameActivityExtension _gameActivities;
+		private readonly HowLongToBeatExtension _howLongToBeatExtension;
+		private readonly StartupSettingsValidator _startupSettingsValidator;
+		private readonly TotalScoreCalculator _totalScoreCalculator;
+		private readonly DateTimeProvider _dateTimeProvider = new DateTimeProvider();
 
-        private PlayNextSettingsViewModel _settings;
-        private StartPagePlayNextViewModel _startPagePlayNextViewModel;
-        private PlayNextMainViewModel _playNextMainViewModel;
-        private PlayNextMainView _playNextMainView;
-        private StartPagePlayNextView _startPageView;
+		private PlayNextSettingsViewModel _settings;
+		private StartPagePlayNextViewModel _startPagePlayNextViewModel;
+		private PlayNextMainViewModel _playNextMainViewModel;
+		private PlayNextMainView _playNextMainView;
+		private StartPagePlayNextView _startPageView;
 
-        public override Guid Id { get; } = Guid.Parse("05234f92-39d3-4432-98c1-6f37a3e4b870");
+		public override Guid Id { get; } = Guid.Parse("05234f92-39d3-4432-98c1-6f37a3e4b870");
 
-        public GameActivityExtension GameActivityExtension => _gameActivities;
-        public HowLongToBeatExtension HowLongToBeatExtension => _howLongToBeatExtension;
+		public GameActivityExtension GameActivityExtension => _gameActivities;
+		public HowLongToBeatExtension HowLongToBeatExtension => _howLongToBeatExtension;
 
-        public PlayNext(IPlayniteAPI api) : base(api)
-        {
-            Api = api;
-            Properties = new GenericPluginProperties
-            {
-                HasSettings = true,
-            };
+		public PlayNext(IPlayniteAPI api) : base(api)
+		{
+			Api = api;
+			Properties = new GenericPluginProperties
+			{
+				HasSettings = true,
+			};
 
-            _gameActivities = GameActivityExtension.Create(_dateTimeProvider, api.Paths.ExtensionsDataPath);
-            _howLongToBeatExtension = HowLongToBeatExtension.Create(api.Paths.ExtensionsDataPath);
+			_gameActivities = GameActivityExtension.Create(_dateTimeProvider, api.Paths.ExtensionsDataPath);
+			_howLongToBeatExtension = HowLongToBeatExtension.Create(api.Paths.ExtensionsDataPath);
 
-            _totalScoreCalculator = new TotalScoreCalculator(this);
+			_totalScoreCalculator = new TotalScoreCalculator(this);
 
-            var pluginSettingsPersistence = new PluginSettingsPersistence(this);
-            _startupSettingsValidator = new StartupSettingsValidator(pluginSettingsPersistence, new SettingsMigrator(pluginSettingsPersistence));
-        }
+			var pluginSettingsPersistence = new PluginSettingsPersistence(this);
+			_startupSettingsValidator = new StartupSettingsValidator(pluginSettingsPersistence, new SettingsMigrator(pluginSettingsPersistence));
+		}
 
-        public static IPlayniteAPI Api { get; private set; }
+		public static IPlayniteAPI Api { get; private set; }
 
-        public override IEnumerable<SidebarItem> GetSidebarItems()
-        {
-            yield return new SidebarItem
-            {
-                Title = ResourceProvider.GetString("LOC_PlayNext_PluginName"),
-                Icon = new TextBlock()
-                {
-                    Text = "\u23ED",
-                    FontFamily = ResourceProvider.GetResource("FontIcoFont") as FontFamily
-                },
-                Type = SiderbarItemType.View,
-                Opened = () =>
-                {
-                    if (_playNextMainView == null || _playNextMainViewModel == null)
-                    {
-                        _playNextMainViewModel = new PlayNextMainViewModel(this);
-                        _playNextMainView = new PlayNextMainView(_playNextMainViewModel);
-                        RefreshPlayNextData();
-                    }
+		public override IEnumerable<SidebarItem> GetSidebarItems()
+		{
+			yield return new SidebarItem
+			{
+				Title = ResourceProvider.GetString("LOC_PlayNext_PluginName"),
+				Icon = new TextBlock()
+				{
+					Text = "\u23ED",
+					FontFamily = ResourceProvider.GetResource("FontIcoFont") as FontFamily
+				},
+				Type = SiderbarItemType.View,
+				Opened = () =>
+				{
+					if (_playNextMainView == null || _playNextMainViewModel == null)
+					{
+						_playNextMainViewModel = new PlayNextMainViewModel(this);
+						_playNextMainView = new PlayNextMainView(_playNextMainViewModel);
+						RefreshPlayNextData();
+					}
 
-                    return _playNextMainView;
-                }
-            };
-        }
+					return _playNextMainView;
+				}
+			};
+		}
 
-        public override void OnGameInstalled(OnGameInstalledEventArgs args)
-        {
-            _startPagePlayNextViewModel?.UpdateGame(args.Game);
-        }
+		public override void OnGameInstalled(OnGameInstalledEventArgs args)
+		{
+			_startPagePlayNextViewModel?.UpdateGame(args.Game);
+		}
 
-        public override void OnGameStarted(OnGameStartedEventArgs args)
-        {
-            // Add code to be executed when game is started running.
-        }
+		public override void OnGameStarted(OnGameStartedEventArgs args)
+		{
+			// Add code to be executed when game is started running.
+		}
 
-        public override void OnGameStarting(OnGameStartingEventArgs args)
-        {
-            // Add code to be executed when game is preparing to be started.
-        }
+		public override void OnGameStarting(OnGameStartingEventArgs args)
+		{
+			// Add code to be executed when game is preparing to be started.
+		}
 
-        public override void OnGameStopped(OnGameStoppedEventArgs args)
-        {
-            RefreshPlayNextData();
-        }
+		public override void OnGameStopped(OnGameStoppedEventArgs args)
+		{
+			RefreshPlayNextData();
+		}
 
-        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
-        {
-            _startPagePlayNextViewModel?.UpdateGame(args.Game);
-        }
+		public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+		{
+			_startPagePlayNextViewModel?.UpdateGame(args.Game);
+		}
 
-        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
-        {
-            _startupSettingsValidator.EnsureCorrectVersionSettingsExist();
-            LandingPageExtension.CreateInstance(PlayniteApi);
-        }
+		public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+		{
+			_startupSettingsValidator.EnsureCorrectVersionSettingsExist();
+			LandingPageExtension.CreateInstance(PlayniteApi);
+		}
 
-        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
-        {
-            // Add code to be executed when Playnite is shutting down.
-        }
+		public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+		{
+			// Add code to be executed when Playnite is shutting down.
+		}
 
-        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
-        {
-            // Add code to be executed when library is updated.
-        }
+		public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+		{
+			// Add code to be executed when library is updated.
+		}
 
-        public override ISettings GetSettings(bool firstRunSettings)
-        {
-            return _settings ?? (_settings = new PlayNextSettingsViewModel(this));
-        }
+		public override ISettings GetSettings(bool firstRunSettings)
+		{
+			return _settings ?? (_settings = new PlayNextSettingsViewModel(this));
+		}
 
-        public override UserControl GetSettingsView(bool firstRunSettings)
-        {
-            return new PlayNextSettingsView();
-        }
+		public override UserControl GetSettingsView(bool firstRunSettings)
+		{
+			return new PlayNextSettingsView();
+		}
 
-        public StartPageExtensionArgs GetAvailableStartPageViews()
-        {
-            return new StartPageExtensionArgs()
-            {
-                ExtensionName = ResourceProvider.GetString("LOC_PlayNext_PluginName"),
-                Views = new[]
-                {
-                    new StartPageViewArgsBase
-                    {
-                        ViewId = "PlayNext_TopRecommendations",
-                        Name = ResourceProvider.GetString("LOC_PlayNext_StartPageTopRecommendationsViewName"),
-                        Description = ResourceProvider.GetString("LOC_PlayNext_StartPageTopRecommendationsDescription")
-                    }
-                }
-            };
-        }
+		public StartPageExtensionArgs GetAvailableStartPageViews()
+		{
+			return new StartPageExtensionArgs()
+			{
+				ExtensionName = ResourceProvider.GetString("LOC_PlayNext_PluginName"),
+				Views = new[]
+				{
+					new StartPageViewArgsBase
+					{
+						ViewId = "PlayNext_TopRecommendations",
+						Name = ResourceProvider.GetString("LOC_PlayNext_StartPageTopRecommendationsViewName"),
+						Description = ResourceProvider.GetString("LOC_PlayNext_StartPageTopRecommendationsDescription")
+					}
+				}
+			};
+		}
 
-        public object GetStartPageView(string viewId, Guid instanceId)
-        {
-            switch (viewId)
-            {
-                case "PlayNext_TopRecommendations":
-                    if (_startPagePlayNextViewModel == null || _startPageView == null)
-                    {
-                        var settings = LoadPluginSettings<PlayNextSettings>();
-                        _startPagePlayNextViewModel = new StartPagePlayNextViewModel(this);
-                        _startPageView = new StartPagePlayNextView(_startPagePlayNextViewModel, settings);
-                        RefreshPlayNextData();
-                    }
+		public object GetStartPageView(string viewId, Guid instanceId)
+		{
+			switch (viewId)
+			{
+				case "PlayNext_TopRecommendations":
+					if (_startPagePlayNextViewModel == null || _startPageView == null)
+					{
+						var settings = LoadPluginSettings<PlayNextSettings>();
+						_startPagePlayNextViewModel = new StartPagePlayNextViewModel(this);
+						_startPageView = new StartPagePlayNextView(_startPagePlayNextViewModel, settings);
+						RefreshPlayNextData();
+					}
 
-                    return _startPageView;
-            }
+					return _startPageView;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        public Control GetStartPageViewSettings(string viewId, Guid instanceId)
-        {
-            return null;
-        }
+		public Control GetStartPageViewSettings(string viewId, Guid instanceId)
+		{
+			return null;
+		}
 
-        public void OnViewRemoved(string viewId, Guid instanceId)
-        {
-        }
+		public void OnViewRemoved(string viewId, Guid instanceId)
+		{
+		}
 
-        public void OnPlayNextSettingsSaved()
-        {
-            var settings = LoadPluginSettings<PlayNextSettings>();
-            _startPagePlayNextViewModel?.UpdateLabelDisplay(settings);
-            _startPageView?.UpdateMinCoverCount(settings);
-            RefreshPlayNextData();
-        }
+		public void OnPlayNextSettingsSaved()
+		{
+			var settings = LoadPluginSettings<PlayNextSettings>();
+			_startPagePlayNextViewModel?.UpdateLabelDisplay(settings);
+			_startPageView?.UpdateMinCoverCount(settings);
+			RefreshPlayNextData();
+		}
 
-        private void RefreshPlayNextData()
-        {
-            new Task<Task>(async () =>
-            {
-                try
-                {
-                    var savedSettings = LoadPluginSettings<PlayNextSettings>();
-                    var recentDayCount = savedSettings.RecentDays;
-                    var gameLengthWeight = savedSettings.GameLengthWeight;
+		private void RefreshPlayNextData()
+		{
+			new Task<Task>(async () =>
+			{
+				try
+				{
+					var savedSettings = LoadPluginSettings<PlayNextSettings>();
+					var recentDayCount = savedSettings.RecentDays;
+					var gameLengthWeight = savedSettings.GameLengthWeight;
 
-                    var allGames = PlayniteApi.Database.Games.ToArray();
-                    var playedGames = new WithPlaytimeFilter().Filter(allGames);
-                    var recentGames = new RecentlyPlayedFilter(_dateTimeProvider).Filter(playedGames, recentDayCount);
-                    var unPlayedGames = new UnplayedFilter().Filter(allGames, savedSettings).ToArray();
+					var allGames = PlayniteApi.Database.Games.ToArray();
+					var playedGames = new WithPlaytimeFilter().Filter(allGames);
+					var recentGames = new RecentlyPlayedFilter(_dateTimeProvider).Filter(playedGames, recentDayCount);
+					var unPlayedGames = new UnplayedFilter().Filter(allGames, savedSettings).ToArray();
 
-                    var activitiesTask = recentGames.Any()
-                        ? _gameActivities.ParseGameActivity(recentGames)
-                        : Task.CompletedTask;
-                    var howLongToBeatTask = gameLengthWeight > 0
-                        ? _howLongToBeatExtension.ParseFiles(unPlayedGames)
-                        : Task.CompletedTask;
+					var activitiesTask = recentGames.Any()
+						? _gameActivities.ParseGameActivity(recentGames)
+						: Task.CompletedTask;
+					var howLongToBeatTask = gameLengthWeight > 0
+						? _howLongToBeatExtension.ParseFiles(unPlayedGames)
+						: Task.CompletedTask;
 
-                    await Task.WhenAll(activitiesTask, howLongToBeatTask);
+					await Task.WhenAll(activitiesTask, howLongToBeatTask);
 
-                    var games = _totalScoreCalculator.Calculate(savedSettings);
+					var games = _totalScoreCalculator.Calculate(savedSettings);
 
-                    _playNextMainViewModel?.LoadData(games);
-                    _startPagePlayNextViewModel?.LoadData(games);
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error(ex, "Failure while refreshing data.");
-                }
-            }).Start();
-        }
-    }
+					_playNextMainViewModel?.LoadData(games);
+					_startPagePlayNextViewModel?.LoadData(games);
+				}
+				catch (Exception ex)
+				{
+					_logger.Error(ex, "Failure while refreshing data.");
+				}
+			}).Start();
+		}
+	}
 }
