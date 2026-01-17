cb10ee40b180cf23902c3c8120131910d0ae8e12
SparrowBrain <qwxmail@gmail.com>
2026-01-17 15:32:41 +0200

    Game details is now listview

--- PlayNext/Model/Score/GameScore/GameLengthCalculator.cs
+++ PlayNext/Model/Score/GameScore/GameLengthCalculator.cs
@@ -4,19 +4,19 @@ using System.Linq;
 
 namespace PlayNext.Model.Score.GameScore
 {
-    public class GameLengthCalculator
-    {
-        public Dictionary<Guid, float> Calculate(Dictionary<Guid, int> games, TimeSpan length)
-        {
-            if (games == null || !games.Any())
-            {
-                return new Dictionary<Guid, float>();
-            }
+	public class GameLengthCalculator
+	{
+		public Dictionary<Guid, float> Calculate(Dictionary<Guid, int> games, TimeSpan length)
+		{
+			if (games == null || !games.Any())
+			{
+				return new Dictionary<Guid, float>();
+			}
 
-            var preferredLengthInSeconds = (int)length.TotalSeconds;
-            var deviation = preferredLengthInSeconds == 0 ? 3600 : preferredLengthInSeconds / 2;
+			var preferredLengthInSeconds = (int)length.TotalSeconds;
+			var deviation = preferredLengthInSeconds <= 1 ? 3600 : preferredLengthInSeconds / 2;
 
-            return games.ToDictionary(x => x.Key, x => (deviation - Math.Min(deviation, Math.Abs(x.Value - preferredLengthInSeconds))) * 100f / deviation);
-        }
-    }
+			return games.ToDictionary(x => x.Key, x => (deviation - Math.Min(deviation, Math.Abs(x.Value - preferredLengthInSeconds))) * 100f / deviation);
+		}
+	}
 }
--- PlayNext/ViewModels/GameToPlayViewModel.cs
+++ PlayNext/ViewModels/GameToPlayViewModel.cs
@@ -1,48 +1,50 @@
-﻿using System;
-using System.Windows.Input;
-using Playnite.SDK;
+﻿using Playnite.SDK;
 using Playnite.SDK.Models;
+using System;
+using System.Windows.Input;
 
 namespace PlayNext.ViewModels
 {
-    public class GameToPlayViewModel
-    {
-        private readonly PlayNext _plugin;
-        private readonly Game _game;
+	public class GameToPlayViewModel
+	{
+		private readonly PlayNext _plugin;
+		private readonly Game _game;
 
-        public GameToPlayViewModel(PlayNext plugin, Game game, float score)
-        {
-            _plugin = plugin;
-            _game = game;
+		public GameToPlayViewModel(PlayNext plugin, Game game, float score)
+		{
+			_plugin = plugin;
+			_game = game;
 
-            Id = game.Id;
-            Name = game.Name;
-            Score = score;
-            Icon = game.Icon;
-            CoverImage = game.CoverImage;
-        }
+			Id = game.Id;
+			Name = game.Name;
+			Score = score;
+			Icon = game.Icon;
+			CoverImage = game.CoverImage;
+		}
 
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public float Score { get; }
+		public Guid Id { get; set; }
 
-        public string CoverImage { get; set; }
+		public string Name { get; set; }
 
-        public string Icon { get; set; }
+		public float Score { get; }
 
-        public ICommand OpenDetails =>
-            new RelayCommand(() =>
-            {
-                _plugin.PlayniteApi.MainView.SelectGame(Id);
-                _plugin.PlayniteApi.MainView.SwitchToLibraryView();
-            });
+		public string CoverImage { get; set; }
 
-        public ICommand RunGame =>
-            new RelayCommand(() =>
-            {
-                _plugin.PlayniteApi.StartGame(Id);
-            });
+		public string Icon { get; set; }
 
-        public bool IsInstalled => _game.IsInstalled;
-    }
+		public ICommand OpenDetails =>
+			new RelayCommand(() =>
+			{
+				_plugin.PlayniteApi.MainView.SelectGame(Id);
+				_plugin.PlayniteApi.MainView.SwitchToLibraryView();
+			});
+
+		public ICommand RunGame =>
+			new RelayCommand(() =>
+			{
+				_plugin.PlayniteApi.StartGame(Id);
+			});
+
+		public bool IsInstalled => _game.IsInstalled;
+	}
 }
