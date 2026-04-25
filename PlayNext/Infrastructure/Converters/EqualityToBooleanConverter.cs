b1f61b1dbf895f61cf4c73036b8e2854f22ae422
David Brett <davidbrett2002@gmail.com>
2026-04-25 20:04:40 +0200

    Add EqualityToBooleanConverter

---
+++ PlayNext/Infrastructure/Converters/EqualityToBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PlayNext.Infrastructure.Converters
+{
+    internal class EqualityToBooleanConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && boolValue)
+            {
+                return parameter;
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
